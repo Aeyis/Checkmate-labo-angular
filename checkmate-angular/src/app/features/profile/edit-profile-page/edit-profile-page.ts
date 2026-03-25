8fc45b70de37ee1a2437f9ef90224e68979f4dd4
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-03-25 14:47:06 +0100

    Profile

--- checkmate-angular/src/app/features/profile/edit-profile-page/edit-profile-page.ts
+++ checkmate-angular/src/app/features/profile/edit-profile-page/edit-profile-page.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {MemberService} from '@core/services/member.service';
+import {Member} from '@core/models/member.interface';
 
 @Component({
     selector: 'app-edit-profile-page',
-    imports: [],
+    imports: [RouterLink, ReactiveFormsModule],
     templateUrl: './edit-profile-page.html',
     styleUrl: './edit-profile-page.css',
 })
-export class EditProfilePage {}
+export class EditProfilePage {
+  private readonly _memberService= inject(MemberService);
+  private readonly _router  = inject(Router) ;
+  private readonly _fb = inject(FormBuilder);
+
+  member: Member | null = null;
+
+  profileForm=this._fb.group({
+    username: [''],
+    email: [''],
+    password: [''],
+    birthdate: [''],
+    gender: [''],
+  })
+/*TODO async ngOnInit(): Promise<void> {
+    this.member = await this._memberService.getMember();
+    this.profileForm matchValue(this.member);
+ }*/
+}
