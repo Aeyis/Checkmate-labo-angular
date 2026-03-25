38249b84c31b75977ad86c2bb1db95105038e4d0
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-03-25 11:19:40 +0100

    ajout errors dans interface et dans route

--- checkmate-angular/src/app/app.routes.ts
+++ checkmate-angular/src/app/app.routes.ts
@@ -1,14 +1,38 @@
 import { Routes } from '@angular/router';
+import {isConnectedGuard} from '@core/guards/isConnected.guard';
+import {isNotConnectedGuard} from '@core/guards/isNotConnected.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
     loadComponent:() =>
       import('@features/auth/login-page/login-page').then((m) => m.LoginPage),
+      canActivate:[isConnectedGuard],
   },
   {
     path: 'register',
     loadComponent:() =>
       import('@features/auth/register-page/register-page').then((m) => m.RegisterPage),
+      canActivate:[isNotConnectedGuard],
+  },
+  {
+    path: 'dashboard',
+    loadComponent:() =>
+      import('@features/dashboard/dashboard-page/dashboard-page').then((m) => m.DashboardPage),
+      canActivate:[isConnectedGuard],
+  },
+  {
+    path: 'forbidden',
+    loadComponent:() =>
+      import('@features/errors/forbidden-page/forbidden-page').then((m) => m.ForbiddenPage),
+  },
+  {
+    path: 'not-found',
+    loadComponent:() =>
+      import('@features/errors/not-found-page/not-found-page').then((m) => m.NotFoundPage),
+  },
+  {
+    path: "**",
+    redirectTo: 'not-found',
   },
 ];
