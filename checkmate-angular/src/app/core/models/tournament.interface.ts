209e3fe9391defb5ae5964b43801291b5cbc0bdc
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-03-31 16:08:09 +0200

    affichage des joueurs inscrits dans le tournoi

--- checkmate-angular/src/app/core/models/tournament.interface.ts
+++ checkmate-angular/src/app/core/models/tournament.interface.ts
@@ -14,6 +14,7 @@ export interface Tournament {
   categories: string[];
   isRegistered: boolean;
   canRegister: boolean;
+  players?: MatchPlayer[];
 }
 export interface TournamentList{
   data: Tournament[];
