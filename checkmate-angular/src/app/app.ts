3165a433ed28973956871c52104aa002eb1897f0
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-04-09 14:09:00 +0200

    opti des img, opti du chargement, et nouveau loader

--- checkmate-angular/src/app/app.ts
+++ checkmate-angular/src/app/app.ts
@@ -1,17 +1,20 @@
 import { Component, computed, inject, signal } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { NavBar } from './shared/layout/nav-bar/nav-bar';
+import { Loading } from './shared/components/loading/loading';
+import { LoadingService } from './core/services/loading.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, Loading],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   private readonly _router = inject(Router);
+  protected readonly loadingService = inject(LoadingService);
 
   protected readonly title = signal('checkmate-angular');
 
@@ -24,4 +27,11 @@ export class App {
   );
 
   protected readonly isAuthPage = computed(() => this._url().startsWith('/auth'));
+
+  constructor() {
+    this._router.events.subscribe(event => {
+      if (event instanceof NavigationStart) this.loadingService.show();
+      if (event instanceof NavigationEnd || event instanceof NavigationError) this.loadingService.hide();
+    });
+  }
 }
